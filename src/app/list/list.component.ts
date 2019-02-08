1830339ed60f20f6247ad7daf988e8c2a470c774
Nate McDowell <nateclaudemcdowel@gmail.com>
2019-02-08 11:43:03 -0500

    Fixing lint errors

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,21 +10,21 @@ export class ListComponent implements OnInit {
   public selectedProviders = [];
   public unselectedProviders = [
     {
-      id: "1",
-      name: "John",
-      address: "123 Greenway Blvd",
+      id: '1',
+      name: 'John',
+      address: '123 Greenway Blvd',
       phone: '8991234321'
     },
     {
-      id: "2",
-      name: "Mary",
-      address: "443 Windwhisper Road",
+      id: '2',
+      name: 'Mary',
+      address: '443 Windwhisper Road',
       phone: '2233211903'
     },
     {
-      id: "3",
-      name: "Jason",
-      address: "9992 Pumpkin Hollow",
+      id: '3',
+      name: 'Jason',
+      address: '9992 Pumpkin Hollow',
       phone: '4343219384'
     }
   ];
